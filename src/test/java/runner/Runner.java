6f9b08415afe44c79ca137f58dc8cc4503982a38
Göksel Celik <gokselcelik5255@gmail.com>
2023-12-22 12:05:55 +0300

    22.12.2023 US10 StepDefs

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         monochrome = true,//raporlarin consoleda okunakli sekilde cikmasi icin
         features="src/test/resources/features",
         glue= {"stepdefinitions", "hooks"},
-        tags="@US10"  ,
+        tags="@US02"  ,
         dryRun= false
 )
 public class Runner {
