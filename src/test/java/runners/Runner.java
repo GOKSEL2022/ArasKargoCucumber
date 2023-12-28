65e16fa45dca45ad956715a2df5e5028ca7e533f
Alparslan <maslan11alp@gmail.com>
2023-12-29 01:43:21 +0300

    29122023_JenkinsIcınSmokeTag

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         monochrome = true,//raporlarin consoleda okunakli sekilde cikmasi icin
         features="src/test/resources/features",
         glue= {"stepdefinitions", "hooks"},
-        tags="@us111"  ,
+        tags="@smoke"  ,
         dryRun= false
 )
 public class Runner {
