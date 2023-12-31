7a76918cba2dbf1e981c8ea0f60cfb0add63a6d5
Alparslan <maslan11alp@gmail.com>
2023-12-31 13:46:16 +0300

    31122023-screenShotFixed

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,8 +1,8 @@
 package hooks;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
-import org.junit.After;
-import org.junit.Before;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import utilities.Driver;
